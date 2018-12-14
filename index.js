0ab952f8effb34eb90ffe1fb79297b5e68cb259a
alasalle <angela.lasalle91@gmail.com>
2018-12-13 22:45:02 -0500

    add endpoints to index.js

--- index.js
+++ index.js
@@ -11,22 +11,64 @@ const PORT = 4000;
 server.use(express.json());
 
 server.post('/crayons', (req, res) => {
-
+  const crayon = req.body;
+  db('crayons').insert(crayon)
+  .then(ids => {
+    res.status(201).json(ids);
+  })
+  .catch(err => {
+    res.status(500).json({error: "failed to add crayon"})
+  })
 });
 
 server.get('/crayons', (req, res) => {
-
+  db('crayons')
+  .then(rows => {
+    res.json(rows)
+  })
+  .catch(err => {
+    res.status(500).json({error: "failed to retrieve crayons"})
+  })
 });
 
 server.get('/crayons/:id', (req, res) => {
-
+  const { id } = req.params;
+    db('crayons')
+    .where('id', id)
+  .then(rows => {
+    res.json(rows)
+  })
+  .catch(err => {
+    res.status(500).json({error: "failed to retrieve crayon"})
+  })
 });
 
 server.put('/crayons/:id', (req, res) => {
+  const { id } = req.params;
+  const crayon = req.body;
+  db('crayons')
+  .where('id', id)
+  .update(crayon)
+  .then(rows => {
+    res.status(200).json(rows);
+  })
+  .catch(err => {
+    res.status(500).json({error: "failed to update crayon"})
+  })
 
 });
 
 server.delete('/crayons/:id', (req, res) => {
+  const { id } = req.params;
+  db('crayons')
+  .where('id', id)
+  .del()
+  .then(rows => {
+    res.status(200).json(rows)
+  }) 
+  .catch(err => {
+    res.status(500).json({error: "failed to delete crayon"})
+  })
 
 });
 
